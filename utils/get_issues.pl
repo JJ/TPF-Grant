e3cb5788bec528e5c8da7d1152f2412634934adf
JJ Merelo <jjmerelo@gmail.com>
2018-04-10 20:28:10 +0200

    Adds data on all issues

---
+++ utils/get_issues.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl6
+
+use v6;
+
+use lib <../../../forks/perl6/perl6-WebService-GitHub/lib>;
+use WebService::GitHub;
+use WebService::GitHub::Issues;
+use JSON::Fast;
+
+die 'export GH_TOKEN' unless %*ENV<GH_TOKEN>;
+
+my $gh = WebService::GitHub::Issues.new;
+my $repo = "perl6/doc";
+
+my @issues = $gh.show( repo => $repo, state => 'all' ).Array;
+spurt "../data/issues/all.json", to-json( @issues );
+
+for @issues -> $issue {
+    say $issue;
+    die "Limit exceeded in issue ", $issue<number>, " please use auth" if !rate-limit-remaining();
+    my $this-issue = $gh.single-issue( repo => $repo, issue => $issue<number> );
+    CATCH {
+	default {
+	    say "Problems with ", $issue<number>;
+	    say .Str;
+	    next;
+	}
+    }
+				   
+    for $this-issue.kv -> $k, $value { # merge issues
+	if ( ! $issue{$k} ) {
+	    $issue{$k} = $value;
+	}
+    }
+    spurt "../data/issues/"~$issue<number>~".json", to-json( $issue );
+    
+}
+
+
+
