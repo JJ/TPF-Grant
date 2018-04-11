d9a9750de599910c937dcf9a09c7ad23a17f6bda
JJ Merelo <jjmerelo@gmail.com>
2018-04-11 12:09:37 +0200

    Closers too

--- utils/chart-data.R
+++ utils/chart-data.R
@@ -4,4 +4,5 @@ library(ggthemes)
 data <- read.csv("../data/issues.csv")
 opens.gt.1 <- data[data$created_by %in%  names( table(data$created_by)[table(data$created_by) > 1]),]
 ggplot(opens.gt.1,aes(x=reorder(created_by,-table(created_by)[created_by])))+geom_bar()+scale_y_log10()+ coord_flip()+theme_tufte()
-ggplot(data,aes(x=reorder(closer,-table(closer)[closer])))+geom_bar()+scale_y_log10()+ coord_flip()+theme_tufte()
+closes.gt.1 <- data[data$closer %in%  names( table(data$closer)[table(data$closer) > 1]),]
+ggplot(closes.gt.1,aes(x=reorder(closer,-table(closer)[closer])))+geom_bar()+scale_y_log10()+ coord_flip()+theme_tufte()
