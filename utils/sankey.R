81e336b023ca2836df664ae103e73fe9ef4d2ee2
JJ Merelo <jjmerelo@gmail.com>
2018-04-11 13:03:54 +0200

    Force network

--- utils/sankey.R
+++ utils/sankey.R
@@ -1,8 +1,13 @@
+library("igraph")
 library("networkD3")
 
 data <- read.csv("../data/issues.csv")
-opens.gt.1 <- data[data$created_by %in%  names( table(data$created_by)[table(data$created_by) > 1]),]
-ggplot(opens.gt.1,aes(x=reorder(created_by,-table(created_by)[created_by])))+geom_bar()+scale_y_log10()+ coord_flip()+theme_tufte()
-closes.gt.1 <- data[data$closer %in%  names( table(data$closer)[table(data$closer) > 1]),]
-closes.gt.1 <- closes.gt.1[closes.gt.1$state == " closed",]
-ggplot(closes.gt.1,aes(x=reorder(closer,-table(closer)[closer])))+geom_bar()+scale_y_log10()+ coord_flip()+theme_tufte()
+closes <- data[data$state == " closed",]
+relations <- data.frame(from=closes$created_by,
+                        to=closes$closer);
+g <- graph_from_data_frame( relations,directed=F, vertices=union(relations$from,relations$to))
+group <- cluster_louvain( g )
+membership <- membership(group)
+g.d3 <- igraph_to_networkD3(g,group=membership)
+forceNetwork(Links=g.d3$links,Nodes=g.d3$nodes,Source='source',Target='target',NodeID='name',Group='group') %>% saveNetwork(file = '../html/force-network-perl6-doc-issues.html')
+sankeyNetwork(Links=g.d3$links,Nodes=g.d3$nodes,Source='source',Target='target',NodeID='name',nodewidth=30)
