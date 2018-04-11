81c7adc9842af759df796de8f7a25271702aa20c
JJ Merelo <jjmerelo@gmail.com>
2018-04-11 12:27:17 +0200

    Adds opener-closer graph

--- utils/chart-data.R
+++ utils/chart-data.R
@@ -5,4 +5,5 @@ data <- read.csv("../data/issues.csv")
 opens.gt.1 <- data[data$created_by %in%  names( table(data$created_by)[table(data$created_by) > 1]),]
 ggplot(opens.gt.1,aes(x=reorder(created_by,-table(created_by)[created_by])))+geom_bar()+scale_y_log10()+ coord_flip()+theme_tufte()
 closes.gt.1 <- data[data$closer %in%  names( table(data$closer)[table(data$closer) > 1]),]
+closes.gt.1 <- closes.gt.1[closes.gt.1$state == " closed",]
 ggplot(closes.gt.1,aes(x=reorder(closer,-table(closer)[closer])))+geom_bar()+scale_y_log10()+ coord_flip()+theme_tufte()
