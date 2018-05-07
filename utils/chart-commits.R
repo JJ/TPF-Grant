65c5ba32db31fcc9956f5469a76c8e4981285c0d
JJ Merelo <jjmerelo@gmail.com>
2018-05-07 10:37:17 +0200

    Adds commits per author ratio

--- utils/chart-commits.R
+++ utils/chart-commits.R
@@ -6,3 +6,5 @@ commits.t <- read.csv("../data/commits.csv")
 commits.t$Month <- as.Date(commits.t$Month)
 ggplot(commits.t, aes(x=Month,y=Commits))+geom_line()+theme_tufte()
 ggplot(commits.t, aes(x=Month,y=Commits))+geom_line()+geom_point(aes(size=Authors,color=Entropy))+theme_tufte()
+commits.t$Ratio <- commits.t$Commits / commits.t$Author
+ggplot(commits.t, aes(x=Month,y=Ratio))+geom_line()+theme_tufte()
