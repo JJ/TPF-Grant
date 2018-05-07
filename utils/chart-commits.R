b38c2f17ddfdfab6916e31e230eaafdd39133624
JJ Merelo <jjmerelo@gmail.com>
2018-05-07 10:28:37 +0200

    Adds entropy

--- utils/chart-commits.R
+++ utils/chart-commits.R
@@ -5,3 +5,4 @@ commits.t <- read.csv("../data/commits.csv")
 
 commits.t$Month <- as.Date(commits.t$Month)
 ggplot(commits.t, aes(x=Month,y=Commits))+geom_line()+theme_tufte()
+ggplot(commits.t, aes(x=Month,y=Commits))+geom_line()+geom_point(aes(size=Authors,color=Entropy))+theme_tufte()
