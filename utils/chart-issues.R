0bad7f38004a7b783acef504de09e4c3e1a8c5fd
JJ Merelo <jjmerelo@gmail.com>
2018-05-08 20:19:19 +0200

    Adds timeline for this year

--- utils/chart-issues.R
+++ utils/chart-issues.R
@@ -1,7 +1,9 @@
 library(ggplot2)
 library(ggthemes)
 
-issues.t <- read.csv("../data/issues-timeline.csv")
+issues.t <- read.csv("../data/issues-timeline-posix.csv")
 
-issues.t$Time <- as.Date(issues.t$Time)
+issues.t$Time <- as.POSIXct(issues.t$Time)
 ggplot(issues.t, aes(x=Time,y=Open.Issues))+geom_line()+theme_tufte()
+issues.2018 <- issues.t[issues.t$Time >= "2018-01-01",]
+ggplot(issues.2018, aes(x=Time,y=Open.Issues))+geom_line()+theme_tufte()
