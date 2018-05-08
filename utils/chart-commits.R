1b80cd7dfb9a31e8897877080903842db898d93d
JJ Merelo <jjmerelo@gmail.com>
2018-05-08 13:29:42 +0200

    First rough draft of second report

--- utils/chart-commits.R
+++ utils/chart-commits.R
@@ -8,4 +8,11 @@ ggplot(commits.t, aes(x=Month,y=Commits))+geom_line()+theme_tufte()
 ggplot(commits.t, aes(x=Month,y=Commits))+geom_line()+geom_point(aes(size=Authors,color=Entropy))+theme_tufte()
 commits.t$Ratio <- commits.t$Commits / commits.t$Author
 ggplot(commits.t, aes(x=Month,y=Ratio))+geom_line()+theme_tufte()
-ggplot(commits.t, aes(x=Month,y=Commits,color=Entropy,size=Authors))+geom_line()+theme_tufte()
+ggplot(commits.t, aes(x=Month,y=Entropy,color=Commits,size=Authors))+geom_line()+theme_tufte()
+commits.t$MonthN <- c(commits.t$Commits[2:length(commits.t$Month)],0)
+correlation <-
+    data.frame(entropy=commits.t$Entropy[1:(length(commits.t$Entropy)-1)],
+               commits.n=commits.t$MonthN[1:(length(commits.t$MonthN)-1)])
+ggplot(correlation, aes(x=commits.n,y=entropy))+geom_point()+theme_tufte()
+
+                                                    
