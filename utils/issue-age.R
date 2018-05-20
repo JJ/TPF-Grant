7f9fc9f284927aca33d9d871043695b74791646a
JJ Merelo <jjmerelo@gmail.com>
2018-05-20 09:31:02 +0200

    Adds sorted plot

--- utils/issue-age.R
+++ utils/issue-age.R
@@ -3,6 +3,7 @@ library(ggthemes)
 
 data <- read.csv('../data/age-issues-tags.csv')
 
-data$Label <- as.factor(data$Label)
+fac <- with(data, reorder(Label, Age, median, order = TRUE)) # finds median
+data$Label <- factor(data$Label,levels=levels(fac)) # Orders factors according to median
 data$Age <- data$Age/86400
 ggplot(data,aes(Label,Age))+geom_point(data=data,aes(x=Label, y=Age,fill=State,color=Label,stroke=1),shape=21,position='jitter',size=5)+geom_boxplot()+ scale_y_log10()+theme_tufte()
