d5f88fbded60a4db53db87dd097667e9515271e6
JJ Merelo <jjmerelo@gmail.com>
2018-05-20 09:23:32 +0200

    Boxplot with age and everything

--- utils/issue-age.R
+++ utils/issue-age.R
@@ -3,6 +3,6 @@ library(ggthemes)
 
 data <- read.csv('../data/age-issues-tags.csv')
 
-data$Label <- as.Level(data$Label)
+data$Label <- as.factor(data$Label)
 data$Age <- data$Age/86400
-ggplot(data,aes(x=Label,y=Age,color=Label))+geom_point(position='jitter')+geom_boxplot()+scale_y_log10()+theme_tufte()
+ggplot(data,aes(Label,Age))+geom_point(data=data,aes(x=Label, y=Age,fill=State,color=Label,stroke=1),shape=21,position='jitter',size=5)+geom_boxplot()+ scale_y_log10()+theme_tufte()
