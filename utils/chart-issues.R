be3f8f454f84d8244233ea7b582a7a3904eb7888
JJ Merelo <jjmerelo@gmail.com>
2018-05-31 19:20:49 +0200

    Updates issues and shows open vs. closed

--- utils/chart-issues.R
+++ utils/chart-issues.R
@@ -7,3 +7,7 @@ issues.t$Time <- as.POSIXct(issues.t$Time)
 ggplot(issues.t, aes(x=Time,y=Open.Issues))+geom_line()+theme_tufte()
 issues.2018 <- issues.t[issues.t$Time >= "2018-01-01",]
 ggplot(issues.2018, aes(x=Time,y=Open.Issues))+geom_line()+theme_tufte()
+
+
+issues.m <- read.csv("../data/issues-month.csv")
+ggplot()+geom_line(data=issues.m,aes(x=Month,y=Open,color="Open",group=1))+geom_line(data=issues.m,aes(x=Month,y=Closed,color="Closed",group=1))+theme_tufte()+ theme(axis.text.x = element_text(angle = 90, hjust = 1))
