2de8647f8dc4d5c24684cbca6dc919532aebc4b9
JJ Merelo <jjmerelo@gmail.com>
2018-06-01 20:21:56 +0200

    Modifies to use new file names

--- utils/chart-entropy.R
+++ utils/chart-entropy.R
@@ -1,19 +1,19 @@
 library(ggplot2)
 library(ggthemes)
 
-entropy <- read.csv("../data/file-entropy.csv")
+entropy <- read.csv("../data/perl6-doc-file-entropy.csv")
 
 ggplot(entropy, aes(x=reorder(File,-Entropy),y=Entropy))+geom_point()+theme_tufte()+ scale_y_log10()
 
-authors <- read.csv("../data/file-authors.csv")
+authors <- read.csv("../data/perl6-doc-file-authors.csv")
 
 ggplot(authors, aes(x=reorder(File,-Authors),y=Authors))+geom_point()+theme_tufte()+ scale_y_log10()+ theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-entropy.a <- read.csv("../data/author-entropy.csv")
+entropy.a <- read.csv("../data/perl6-doc-author-entropy.csv")
 
 ggplot(entropy.a, aes(x=reorder(Author,-Entropy),y=Entropy))+geom_point()+theme_tufte()+ scale_y_log10()+ theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-entropy.c <- read.csv("../data/current-files.csv")
+entropy.c <- read.csv("../data/perl6-doc-current-files.csv")
 entropy.c <- entropy.c[ entropy.c$Entropy > 0,]
 ggplot(entropy.c, aes(x=Size,y=Entropy,color=Authors))+geom_point()+theme_tufte()+ theme(axis.text.x = element_text(angle = 90, hjust = 1))+ scale_x_log10()
 ggplot(entropy.c, aes(x=Authors,y=Entropy,color=Size))+geom_point()+theme_tufte()+ theme(axis.text.x = element_text(angle = 90, hjust = 1))+ scale_x_log10()
