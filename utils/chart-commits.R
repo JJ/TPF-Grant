c5da20f27d18a17e73850efb6f9727867419d80d
JJ Merelo <jjmerelo@gmail.com>
2018-06-01 19:24:48 +0200

    Updates commits/month

--- utils/chart-commits.R
+++ utils/chart-commits.R
@@ -3,6 +3,7 @@ library(ggthemes)
 
 commits.t <- read.csv("../data/commits.csv")
 
+commits.t <- commits.t[-nrow(commits.t),]
 commits.t$Month <- as.Date(commits.t$Month)
 ggplot(commits.t, aes(x=Month,y=Commits))+geom_line()+theme_tufte()
 ggplot(commits.t, aes(x=Month,y=Commits))+geom_line()+geom_point(aes(size=Authors,color=Entropy))+theme_tufte()
