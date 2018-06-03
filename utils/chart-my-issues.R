5e8753aab5d5f8a5df389f199bfe7536b9336268
JJ Merelo <jjmerelo@gmail.com>
2018-06-03 18:34:32 +0200

    Edits 1142 to avoid problems

--- utils/chart-my-issues.R
+++ utils/chart-my-issues.R
@@ -3,8 +3,8 @@ library(ggthemes)
 
 data <- read.csv("../data/closed-jj.csv")
 
-data.april <-  data[data$Month == " 2018-04-01T00:00:00",]
-ggplot(data.april, aes(x=Author,y=Closed.Issues, fill=Author))+geom_bar(stat='identity')+theme_tufte()
+data.may <-  data[data$Month == " 2018-05-01T00:00:00",]
+ggplot(data.may, aes(x=Author,y=Closed.Issues, fill=Author))+geom_bar(stat='identity')+theme_tufte()
 
 ages <- read.csv("../data/age-closed.csv")
 ggplot(ages, aes(x=Author,fill=Closed.Issues, y=Avg.Age))+geom_bar(stat='identity')+theme_tufte()
