9474c2f8f79b05486fed095de1cc5d8a38810063
JJ Merelo <jjmerelo@gmail.com>
2018-06-04 19:07:09 +0200

    Ages of issues plott4ed

--- utils/chart-issues.R
+++ utils/chart-issues.R
@@ -9,6 +9,16 @@ ggplot(issues.t)+geom_line(data=issues.t,aes(x=Time,y=Open.Issues,color='Issues'
 issues.2018 <- issues.t[issues.t$Time >= "2018-01-01",]
 ggplot()+geom_line(data=issues.2018,aes(x=Time,y=Open.Issues,color='Issues'))+geom_line(data=issues.2018,aes(x=Time,y=Average.Age,color='Age'))+theme_tufte()
 
+issues.e <- read.csv("../data/issues-timeline-excluded.csv")
+
+issues.e$Time <- as.POSIXct(gsub("T"," ",issues.e$Time))
+
+ggplot(issues.e)+geom_line(data=issues.e,aes(x=Time,y=Open.Issues,color='Issues'))+geom_line(data=issues.e,aes(x=Time,y=Average.Age,color='Age'))+theme_tufte()
+issues.2018.e <- issues.e[issues.e$Time >= "2018-01-01",]
+ggplot()+geom_line(data=issues.2018.e,aes(x=Time,y=Open.Issues,color='Issues'))+geom_line(data=issues.2018.e,aes(x=Time,y=Average.Age,color='Age'))+theme_tufte()
+
+age.diff <- merge(issues.e,issues.t,by="Time")
+ggplot()+geom_line(data=age.diff,aes(x=Time,y=Average.Age.x,color='Excluded'))+geom_line(data=age.diff,aes(x=Time,y=Average.Age.y,color='Included'))+theme_tufte()
 
 issues.m <- read.csv("../data/issues-month.csv")
 issues.m$deficit <- issues.m$Closed - issues.m$Open
