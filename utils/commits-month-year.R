841ae92c5f51350c0a83d6bbaecc92f36c825999
JJ Merelo <jjmerelo@gmail.com>
2018-09-06 18:47:01 +0200

    Adds cumulative plot

--- utils/commits-month-year.R
+++ utils/commits-month-year.R
@@ -6,3 +6,5 @@ commits.t <- commits.t[-nrow(commits.t),] # Last month is not complete
 commits.t$Year <- as.factor(commits.t$Year)
 ggplot(commits.t, aes(x=Month,y=Commits,group=Year))+geom_line(aes(color=commits.t$Year))+geom_point(aes(size=Authors))+theme_tufte()+theme(legend.position="top")
 ggsave("../illos/commits-year-month.png",width=16, height=9)
+ggplot(commits.t, aes(x=Month,y=cumsum(Commits),group=Year))+geom_line(aes(color=commits.t$Year))+geom_point(aes(size=Authors))+theme_tufte()+theme(legend.position="top")
+ggsave("../illos/cumulative-commits-year-month.png",width=16, height=9)
