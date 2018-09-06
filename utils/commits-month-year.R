e50a425a6aeee33436473df0fbae067300ded218
JJ Merelo <jjmerelo@gmail.com>
2018-09-06 18:29:32 +0200

    Adds new chart per year

---
+++ utils/commits-month-year.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+library(ggthemes)
+
+commits.t <- read.csv("../data/commits-year-month.csv")
+commits.t <- commits.t[-nrow(commits.t),] # Last month is not complete
+commits.t$Year <- as.factor(commits.t$Year)
+ggplot(commits.t, aes(x=Month,y=Commits,group=Year))+geom_line(aes(color=commits.t$Year))+geom_point(aes(size=Authors))+theme_tufte()+theme(legend.position="top")
+ggsave("../illos/commits-year-month.png",width=16, height=9)
