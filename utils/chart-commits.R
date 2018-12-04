e70cfd32be9ea9937efd6783e610793bb54ced2a
JJ Merelo <jjmerelo@gmail.com>
2018-12-04 07:46:40 +0100

    Updates ilustrations

--- utils/chart-commits.R
+++ utils/chart-commits.R
@@ -5,8 +5,8 @@ library(ggthemes)
 commits.t <- read.csv("../data/commits.csv")
 
 commits.t$Ratio <- commits.t$Commits / commits.t$Author
-ggplot(commits.t, aes(x=Month,y=Ratio))+geom_line()+theme_tufte()
-ggplot(commits.t, aes(x=Month,y=Entropy,color=Commits,size=Authors))+geom_line()+theme_tufte()
+ggplot(commits.t, aes(x=Month,y=Ratio,group=1))+geom_line()+theme_tufte()
+ggplot(commits.t, aes(x=Month,y=Entropy,color=Commits,size=Authors,group=1))+geom_line()+theme_tufte()
 commits.t$MonthN <- c(commits.t$Commits[2:length(commits.t$Month)],0)
 correlation <-
     data.frame(entropy=commits.t$Entropy[1:(length(commits.t$Entropy)-1)],
