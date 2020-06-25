218f2a13f51ef60d7ca6f2d8ea9fc994c08baf55
JJ Merelo <jjmerelo@gmail.com>
2020-06-25 17:21:18 +0200

    Updating this

--- utils/lines-written.R
+++ utils/lines-written.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(ggthemes)
 
-data <- read.csv("../data/current-lines-written.csv")
+data <- read.csv("../data/current-lines-written.csv",header=TRUE)
 data$idu <- as.numeric(row.names(data))
 ggplot(data,aes(x=idu,y=lines))+geom_point()+scale_y_log10()+scale_y_log10() +theme_tufte()
 data$perc <- 100*data$lines/sum(data$lines)
